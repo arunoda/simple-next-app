44ff9b3028e883bdd17b6f2ee09fe8a7595284d3
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2020-03-27 20:03:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 const Index = () => (
   <div>
-    <p>Hello Next.js From a forked PR 222 sda asdsa dsd adas</p>
+    <p>Hello Next.js From a forked PR 222 sda asdsa dsd adddas</p>
   </div>
 );
+
 
 export default Index;
